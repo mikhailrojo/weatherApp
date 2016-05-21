b614f6b1a7cdda8383d3bd23ce17392b54ada648
misha <misha.stepanov@mail.ru>
2016-05-21 15:49:28 +0300

    Делаю коммит так как сделал автоматическое определение погоды и ее отображение

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,55 @@
-(function(){
-	if("geolocation" in navigator){
-		navigator.geolocation.getCurrentPosition(function(position){
-			console.log('широта ' + position.coords.latitude);
-			console.log('долгота ' + position.coords.longitude);
+angular.module('weatherApp', [])
+	.config(function($interpolateProvider){
+		$interpolateProvider.startSymbol('[[');
+		$interpolateProvider.endSymbol(']]');
 			
-			var json = JSON.stringify({
-				longitude: position.coords.longitude,
-				latitude : position.coords.latitude
-			});
-			
-			var xhr = new XMLHttpRequest();
-			xhr.open('POST', '/geo', true);
-			xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-			xhr.send(json);
-			
-			xhr.onreadystatechange = function(){
-				if(xhr.readyState !=4) return;
-				
-				if(xhr.status != 200){
-					console.log('Статус ' + xhr.status + ':' + xhr.statustext);
-				} else {
-					console.log(xhr.responseText);
-				}
-				
+	})
+	.controller('weatherCtrl', function(geo, $scope, $http){
+		console.log('контроллер');	
+		var result;
+			if("geolocation" in navigator){
+				navigator.geolocation.getCurrentPosition(function(position){
+
+					var longitude = position.coords.longitude;
+					var latitude = position.coords.latitude;
+					var id =  '&lang=ru&units=metric&APPID=ee7b44dbcbd8e281e70c9fd015b08a00';
+					//var query = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude + id;
+					var query = "http://ip-api.com/json/?fields=country,city";
+					
+					
+					$http.get(query)
+						.then(function(res){
+							console.log(res.data.city, "все гуд");
+							console.log(res.data.country, "все гуд");
+							
+							var weatherQuery = "http://api.openweathermap.org/data/2.5/weather?q="+ res.data.city  + "," + res.data.country+ id;
+							$http.get(weatherQuery)
+								.then(function(res){
+										console.log(res.data);
+										$scope.mi = res.data;
+										
+								}, function(res){
+									console.log(res, "ошибка");
+								});
+							
+						},function(){
+							console.log(res, "ошибка");
+						})
+					
+				});
+			} else{
+				console.log('гео нет');
 			}
-		});
+
+	
+})
+	.factory('geo', function($http){
 		
-	} else{
-		console.log('гео нет');
-	}
-})();
+		return function(){
+					console.log("result = ", result);
+			
+		}
+		
+	});
+
+
