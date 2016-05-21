dc5237cf5f51958ef7edf610851cc8b9f1e5a5fa
misha <misha.stepanov@mail.ru>
2016-05-22 01:02:43 +0300

    настроил удаление ненужной погоды

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-angular.module('weatherApp', ['ngCookies'])
+angular.module('weatherApp', ['ngCookies', 'ngAnimate'])
 	.config(function($interpolateProvider){
 		$interpolateProvider.startSymbol('[[');
 		$interpolateProvider.endSymbol(']]');
@@ -7,43 +7,37 @@ angular.module('weatherApp', ['ngCookies'])
 		$scope.panels = $cookies.getObject('previousQueries') || [];
 		var now = new Date();
 		var oneYearCookie = new Date(now.getFullYear()+2, now.getMonth(), now.getDay());
-		console.log($scope.panels);
-		
 		$scope.findWeather = function(chosenCity){
+			$scope.panels = $cookies.getObject('previousQueries') || [];
 			$scope.panels.push(chosenCity);
-			console.log($scope.panels);	
 			$cookies.putObject('previousQueries', $scope.panels, { expires: oneYearCookie});
+			console.log($scope.panels);
 		}
 	})
+	
 	.directive("cityWeather", function($http, $cookies){
 	
 		function link(scope, element, attr){
-		
+			
 			var cross = element.children().children()[0];
 			var cookiesArray = $cookies.getObject('previousQueries');
 			var now = new Date();
 			var oneYearCookie = new Date(now.getFullYear()+2, now.getMonth(), now.getDay());
+			
 			element.on("mousedown", function(e){
-				if(e.target == cross ){
 					
-					var cityScope = scope.$parent.panels;
-					cookiesArray.splice(cookiesArray.indexOf(attr.city), 1);
-					//console.log(cookiesArray);
-					$cookies.putObject('previousQueries', cookiesArray, { expires: oneYearCookie});
-					cityScope = cookiesArray;
-					
-					
-
+				console.log(cookiesArray)
 				
+				if(e.target == cross ){
+					cookiesArray.splice(cookiesArray.indexOf(attr.city), 1);
+					$cookies.putObject('previousQueries', cookiesArray, { expires: oneYearCookie});
+					element.remove();
+					console.log(cookiesArray);
 				}
 			});
 			
 			var positionStored = $cookies.getObject(attr.city) || null;
-		// устанавливаем данные о погоде в Куки, которые истекают каждый 5 минут.
-		// Если они истекли то делается запрос на сервер
-			//console.log(positionStored);
 			if(!positionStored){
-					
 					var id =  '&lang=ru&units=metric&APPID=ee7b44dbcbd8e281e70c9fd015b08a00';
 					var query = "http://ip-api.com/json/?fields=country,city";
 					$http.get(query)
@@ -52,10 +46,8 @@ angular.module('weatherApp', ['ngCookies'])
 							var weatherQuery = "http://api.openweathermap.org/data/2.5/weather?q="+ city  + "," + res.data.country+ id;
 							$http.get(weatherQuery)
 								.then(function(res){
-										//console.log("сделали запрос на сервер");
 										var expiryDate =  new Date(new Date().valueOf() + 1000*60*5)
 										$cookies.putObject(city, res.data, {expires: expiryDate });
-										//console.log($cookies.getAll());
 										scope.mi = res.data;
 										
 								}, function(res){
@@ -69,9 +61,10 @@ angular.module('weatherApp', ['ngCookies'])
 				
 			} else{
 				scope.mi = positionStored;
-				//console.log("Информацию о погоде берем из кукисов");
 			}
-
+		scope.$on('$destroy', function(){
+					element.remove();
+				});
 		}
 		
 		return {
