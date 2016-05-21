da301880a526c9f7678f398a1b4475f8407c4a4c
misha <misha.stepanov@mail.ru>
2016-05-21 22:55:41 +0300

    улучшил дизайн, доделал добавление и сохранение списков выбранных городов. Теперь планирую настроить удаление. Насколько я понимаю чтобы из детского скоупа удалить позицию родительского нужно передавать в ng-repeat объект а не примитив, поменяю примитивы на объекты

--- public/js/main.js
+++ public/js/main.js
@@ -2,28 +2,61 @@ angular.module('weatherApp', ['ngCookies'])
 	.config(function($interpolateProvider){
 		$interpolateProvider.startSymbol('[[');
 		$interpolateProvider.endSymbol(']]');
-			
 	})
-	.controller('weatherCtrl', function(geo, $scope, $http, $cookies){
-		var positionStored = $cookies.getObject('weather') || null;
-			console.log(positionStored);
-			if(!positionStored){
+	.controller('weatherCtrl', function($scope, $cookies){
+		$scope.panels = $cookies.getObject('previousQueries') || [];
+		var now = new Date();
+		var oneYearCookie = new Date(now.getFullYear()+2, now.getMonth(), now.getDay());
+		console.log($scope.panels);
+		
+		$scope.findWeather = function(chosenCity){
+			$scope.panels.push(chosenCity);
+			console.log($scope.panels);	
+			$cookies.putObject('previousQueries', $scope.panels, { expires: oneYearCookie});
+		}
+	})
+	.directive("cityWeather", function($http, $cookies){
+	
+		function link(scope, element, attr){
+		
+			var cross = element.children().children()[0];
+			var cookiesArray = $cookies.getObject('previousQueries');
+			var now = new Date();
+			var oneYearCookie = new Date(now.getFullYear()+2, now.getMonth(), now.getDay());
+			element.on("mousedown", function(e){
+				if(e.target == cross ){
+					
+					var cityScope = scope.$parent.panels;
+					cookiesArray.splice(cookiesArray.indexOf(attr.city), 1);
+					//console.log(cookiesArray);
+					$cookies.putObject('previousQueries', cookiesArray, { expires: oneYearCookie});
+					cityScope = cookiesArray;
+					
+					
+
 				
+				}
+			});
+			
+			var positionStored = $cookies.getObject(attr.city) || null;
+		// устанавливаем данные о погоде в Куки, которые истекают каждый 5 минут.
+		// Если они истекли то делается запрос на сервер
+			//console.log(positionStored);
+			if(!positionStored){
+					
 					var id =  '&lang=ru&units=metric&APPID=ee7b44dbcbd8e281e70c9fd015b08a00';
 					var query = "http://ip-api.com/json/?fields=country,city";
 					$http.get(query)
 						.then(function(res){
-							console.log(res.data.city, "все гуд");
-							console.log(res.data.country, "все гуд");
-							
-							var weatherQuery = "http://api.openweathermap.org/data/2.5/weather?q="+ res.data.city  + "," + res.data.country+ id;
+							var city = attr.city || res.data.city;
+							var weatherQuery = "http://api.openweathermap.org/data/2.5/weather?q="+ city  + "," + res.data.country+ id;
 							$http.get(weatherQuery)
 								.then(function(res){
-										console.log("сделали запрос на сервер");
-										console.log(res.data);
+										//console.log("сделали запрос на сервер");
 										var expiryDate =  new Date(new Date().valueOf() + 1000*60*5)
-										$cookies.putObject('weather', res.data, {expires: expiryDate });
-										$scope.mi = res.data;
+										$cookies.putObject(city, res.data, {expires: expiryDate });
+										//console.log($cookies.getAll());
+										scope.mi = res.data;
 										
 								}, function(res){
 									console.log(res, "ошибка");
@@ -35,19 +68,18 @@ angular.module('weatherApp', ['ngCookies'])
 					
 				
 			} else{
-				$scope.mi = positionStored;
-				console.log("Информацию о погоде берем из кукисов");
+				scope.mi = positionStored;
+				//console.log("Информацию о погоде берем из кукисов");
 			}
 
-	
-	})
-	.factory('geo', function($http){
-		
-		return function(){
-					console.log("result = ", result);
-			
 		}
 		
-	});
+		return {
+			restrict : 'E',
+			templateUrl :'weather',
+			link: link
+		}
+	})
+
 
 
