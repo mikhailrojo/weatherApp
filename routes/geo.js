c011881dc5b30be1bc51fe0ec5a2f89d3bde75bd
misha <misha.stepanov@mail.ru>
2016-05-22 19:12:49 +0300

    Удаленный запрос города по ip

--- routes/geo.js
+++ routes/geo.js
@@ -5,12 +5,27 @@ var http = require('http');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	var ip = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
-     console.log(ip);
-res.json(ip);
+	var ip = 	req.headers['x-forwarded-for'] || 
+     			req.connection.remoteAddress || 
+     			req.socket.remoteAddress ||
+     			req.connection.socket.remoteAddress;
+    var = queryString = "http://ip-api.com/json/" + ip; 
+    
+    
+http.get(queryString, function(res){
+	res.on("data", function(data){
+		console.log(data);
+		res.json(data);
+	});
+	res.resume()
+}).on('error', function(err){
+	console.log(err);
+	res.json(data);
+})'
+
+
+
+
 });
 
 module.exports = router;
